75a0cac008f226ec83b4c28e8ce4c96398910522
Muzammil98 <muzammilsarwar0@gmail.com>
2019-12-17 12:42:39 +0500

    first commit

---
+++ server.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+
+// Routes
+const users = require("./routes/api/users");
+const posts = require("./routes/api/posts");
+const profiles = require("./routes/api/profiles");
+
+// Port Setup
+const port = process.env.PORT || 5000;
+
+//MongoDB Connection
+const uri = require("./config/Keys").mongoURI;
+mongoose
+  .connect(uri, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => {
+    console.log("MongoDB Connected...");
+  })
+  .catch(err => console.log(err));
+
+// Use routes middleware
+app.use("/api/users", users);
+app.use("/api/posts", posts);
+app.use("/api/profiles", profiles);
+
+app.get("/", (req, res) => {
+  res.send("HELLo");
+});
+
+app.listen(port, () => console.log(`Server Running on ${port}...`));
